213336e0ff6f985b3fa5b16e32ba47e1646b9684
TheNuclearNexus <39636175+TheNuclearNexus@users.noreply.github.com>
2024-09-11 13:09:25 -0400

    fix: require model merging

--- smithed/weld/merging/plugin.py
+++ smithed/weld/merging/plugin.py
@@ -1,9 +1,11 @@
 from beet import Context, LootTable
+from beet.contrib import model_merging
 
 from .handler import ConflictsHandler
 
 
 def beet_default(ctx: Context):
+    ctx.require(model_merging.beet_default)
     handler = ctx.inject(ConflictsHandler)
     ctx.data.merge_policy.extend_namespace(LootTable, handler)
 
